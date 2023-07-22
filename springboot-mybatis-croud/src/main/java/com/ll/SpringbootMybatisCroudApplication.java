39ec12402c3184926ec63cee535fdab337c65444
supriselan <71969915+supriselan@users.noreply.github.com>
2023-07-22 19:01:49 +0800

    Add files via upload

---
+++ springboot-mybatis-croud/src/main/java/com/ll/SpringbootMybatisCroudApplication.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisCroudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisCroudApplication.class, args);
+    }
+
+}
